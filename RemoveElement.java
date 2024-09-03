290cc7f20d8458dbe35353c3a806ce04676cfd60
NamCap99 <capmanhnam@gmail.com>
2024-09-03 15:09:26 +0700

    first commit

---
+++ RemoveElement.java
@@ -0,0 +1,28 @@
+package LeetCode.Question2;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        int k = removeElement(nums, val);
+
+        System.out.println("Number of elements not equal to val: " + k);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0; // This will keep track of the index for elements not equal to val
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
+}
